cd380f17e790b6f0941159d52fcee251a88d4c43
Carlo Suriano <carlo.suriano@deustechnology.com>
2019-02-18 12:26:53 +0100

    [rem] wrong combinator

--- structure/ycombinator.py
+++ structure/ycombinator.py
@@ -7,6 +7,4 @@ Y_SHORT = lambda f: lambda *args: f(Y(f))(*args)
 
 Y_LONG = lambda b: ((lambda f: b(lambda *x: f(f)(*x)))((lambda f: b(lambda *x: f(f)(*x)))))
 
-Y_VAR = lambda f: (lambda x: (lambda m: f(x(x))(m)))(lambda x: (lambda m: f(x(x))(m)))
-
 Y_LAZY = lambda f: (lambda x: x(x))(lambda y: f(lambda *args: lambda: y(y)(*args)))
--- tests/test_calculation.py
+++ tests/test_calculation.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import
 import pytest
 from functools import partial
 
-from structure.ycombinator import Y, Y_SHORT, Y_LONG, Y_LAZY, Y_VAR
+from structure.ycombinator import Y, Y_SHORT, Y_LONG, Y_LAZY
 from fibonacci import accumulately, cythonely, iterately, lambdely, recursely
 
 
@@ -13,12 +13,10 @@ from fibonacci import accumulately, cythonely, iterately, lambdely, recursely
     partial(lambdely.calculate, Y),
     partial(lambdely.calculate, Y_SHORT),
     partial(lambdely.calculate, Y_LONG),
-    partial(lambdely.calculate, Y_VAR),
     # TODO partial(lambdely.calculate, Y_LAZY),
     partial(accumulately.calculate, Y),
     partial(accumulately.calculate, Y_SHORT),
     partial(accumulately.calculate, Y_LONG),
-    # TODO partial(accumulately.calculate, Y_VAR),
     partial(accumulately.calculate, Y_LAZY),
 ])
 def calculating_fn(request):
